cac2615b884c0284901b746954d89586425e4c07
Sankethkr24 <sanketh.kr@hyniva.com>
2023-10-03 15:29:51 +0530

    Initial commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const getCertificate = async (event) => {
     const { Item } = await client.send(new GetItemCommand(params));
     response.body = JSON.stringify({
       message: 'Successfully retrieved post.',
-      data: Item ? unmarshall(Item) : {},
+      // data: Item ? unmarshall(Item) : {},
       rawData: Item,
     });
   } catch (e) {
@@ -39,100 +39,6 @@ const getCertificate = async (event) => {
   }
   return response;
 };
-
-// const createPost = async (event) => {
-//   const response = { statusCode: 200 };
-//   try {
-//     const body = JSON.parse(event.body);
-//     const params = {
-//       TableName: process.env.DYNAMODB_TABLE_NAME,
-//       Item: marshall(body || {}),
-//     };
-//     const createResult = await client.send(new PutItemCommand(params));
-//     response.body = JSON.stringify({
-//       message: 'Successfully created post.',
-//       createResult,
-//     });
-//   } catch (e) {
-//     console.error(e);
-//     response.statusCode = 500;
-//     response.body = JSON.stringify({
-//       message: 'Failed to create post.',
-//       errorMsg: e.message,
-//       errorStack: e.stack,
-//     });
-//   }
-//   return response;
-// };
-
-// const updatePost = async (event) => {
-//   const response = { statusCode: 200 };
-//   try {
-//     const body = JSON.parse(event.body);
-//     const objKeys = Object.keys(body);
-//     const params = {
-//       TableName: process.env.DYNAMODB_TABLE_NAME,
-//       Key: marshall({ postId: event.pathParameters.postId }),
-//       UpdateExpression: `SET ${objKeys
-//         .map((_, index) => `#key${index} = :value${index}`)
-//         .join(', ')}`,
-//       ExpressionAttributeNames: objKeys.reduce(
-//         (acc, key, index) => ({
-//           ...acc,
-//           [`#key${index}`]: key,
-//         }),
-//         {}
-//       ),
-//       ExpressionAttributeValues: marshall(
-//         objKeys.reduce(
-//           (acc, key, index) => ({
-//             ...acc,
-//             [`:value${index}`]: body[key],
-//           }),
-//           {}
-//         )
-//       ),
-//     };
-//     const updateResult = await client.send(new UpdateItemCommand(params));
-//     response.body = JSON.stringify({
-//       message: 'Successfully updated post.',
-//       updateResult,
-//     });
-//   } catch (e) {
-//     console.error(e);
-//     response.statusCode = 500;
-//     response.body = JSON.stringify({
-//       message: 'Failed to update post.',
-//       errorMsg: e.message,
-//       errorStack: e.stack,
-//     });
-//   }
-//   return response;
-// };
-
-// const deletePost = async (event) => {
-//   const response = { statusCode: 200 };
-//   try {
-//     const params = {
-//       TableName: process.env.DYNAMODB_TABLE_NAME,
-//       Key: marshall({ postId: event.pathParameters.postId }),
-//     };
-//     const deleteResult = await client.send(new DeleteItemCommand(params));
-//     response.body = JSON.stringify({
-//       message: 'Successfully deleted post.',
-//       deleteResult,
-//     });
-//   } catch (e) {
-//     console.error(e);
-//     response.statusCode = 500;
-//     response.body = JSON.stringify({
-//       message: 'Failed to delete post.',
-//       errorMsg: e.message,
-//       errorStack: e.stack,
-//     });
-//   }
-//   return response;
-// };
 
 const getAllCertificates = async () => {
   const response = { statusCode: 200 };
