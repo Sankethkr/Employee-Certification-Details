2db513acf0bbf328d571344041a3073fffdf20ce
Sankethkr24 <sanketh.kr@hyniva.com>
2023-10-10 12:58:44 +0530

    initial commit

--- app.js
+++ app.js
@@ -11,42 +11,42 @@ const client = new DynamoDBClient();
 
 const getCertificates = async (event) => {
   const response = { statusCode: 200 };
+console.log('event', event);
+try {
+  if (event.pathParameters && event.pathParameters.empID) {
+    // If empID is provided in the path parameters, retrieve a specific certificate
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ empID: event.pathParameters.empID }),
+    };
+    const { Item } = await client.send(new GetItemCommand(params));
 
-  try {
-    if (event.pathParameters && event.pathParameters.empID) {
-      // If empID is provided in the path parameters, retrieve a specific certificate
-      const params = {
-        TableName: process.env.DYNAMODB_TABLE_NAME,
-        Key: marshall({ empID: event.pathParameters.empID }),
-      };
-      const { Item } = await client.send(new GetItemCommand(params));
-
-      response.body = JSON.stringify({
-        message: `Successfully retrieved empId: ${event.pathParameters.empID}`,
-        data: Item ? unmarshall(Item) : {},
-        rawData: Item,
-      });
-    } else {
-      // If empID is not provided, retrieve all certificates
-      const { Items } = await client.send(
-        new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
-      );
-
-      response.body = JSON.stringify({
-        message: 'Successfully retrieved all Certificates',
-        data: Items?.map((item) => unmarshall(item)),
-        Items,
-      });
-    }
-  } catch (e) {
-    console.error(e);
-    response.statusCode = 500;
     response.body = JSON.stringify({
-      message: 'Failed to get certificates.',
-      errorMsg: e.message,
-      errorStack: e.stack,
+      message: `Successfully retrieved empId: ${event.pathParameters.empID}`,
+      data: Item ? unmarshall(Item) : {},
+      rawData: Item,
+    });
+  } else {
+    // If empID is not provided, retrieve all certificates
+    const { Items } = await client.send(
+      new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
+    );
+
+    response.body = JSON.stringify({
+      message: 'Successfully retrieved all Certificates',
+      data: Items?.map((item) => unmarshall(item)),
+      Items,
     });
   }
+} catch (e) {
+  console.error(e);
+  response.body = JSON.stringify({
+    message: 'Failed to get certificates.',
+    errorMsg: e.message,
+    errorStack: e.stack,
+    statusCode: e.$metadata.httpStatusCode,
+  });
+}
 
   return response;
 };
