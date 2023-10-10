128a80830d0b85b86712258b723383715d9baeb6
Sankethkr24 <sanketh.kr@hyniva.com>
2023-10-10 11:49:26 +0530

    Single function

--- app.js
+++ app.js
@@ -1,66 +1,57 @@
+//This program is to retrieve the certification details of employee by empId and all records
 const {
-  DynamoDBClient,
-  GetItemCommand,
-  ScanCommand,
+  DynamoDBClient, //creating of instance of dynamoDB
+  GetItemCommand, //fetch the Item details in dynamoDB
+  ScanCommand, //scan the database
 } = require('@aws-sdk/client-dynamodb');
-const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb'); // retrieve and store
 
+//creating constant client(instance) of dynamoDB and called in the program
 const client = new DynamoDBClient();
 
-const getCertificate = async (event) => {
+const getCertificates = async (event) => {
   const response = { statusCode: 200 };
+
   try {
-    // Check if empId is defined in event.pathParameters
-    if (!event.pathParameters || !event.pathParameters.empId) {
-      throw new Error('empId is missing in the path parameters');
+    if (event.pathParameters && event.pathParameters.empID) {
+      // If empID is provided in the path parameters, retrieve a specific certificate
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ empID: event.pathParameters.empID }),
+      };
+      const { Item } = await client.send(new GetItemCommand(params));
+
+      response.body = JSON.stringify({
+        message: `Successfully retrieved empId: ${event.pathParameters.empID}`,
+        data: Item ? unmarshall(Item) : {},
+        rawData: Item,
+      });
+    } else {
+      // If empID is not provided, retrieve all certificates
+      const { Items } = await client.send(
+        new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
+      );
+
+      response.body = JSON.stringify({
+        message: 'Successfully retrieved all Certificates',
+        data: Items?.map((item) => unmarshall(item)),
+        Items,
+      });
     }
-
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ empId: event.pathParameters.empId }),
-    };
-    const { Item } = await client.send(new GetItemCommand(params));
-    response.body = JSON.stringify({
-      message: 'Successfully retrieved post.',
-      data: Item ? unmarshall(Item) : {},
-      rawData: Item,
-    });
   } catch (e) {
     console.error(e);
     response.statusCode = 500;
     response.body = JSON.stringify({
-      message: 'Failed to get certificate.',
+      message: 'Failed to get certificates.',
       errorMsg: e.message,
       errorStack: e.stack,
     });
   }
+
   return response;
 };
 
-const getAllCertificates = async () => {
-  const response = { statusCode: 200 };
-  try {
-    const { Items } = await client.send(
-      new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
-    );
-    response.body = JSON.stringify({
-      message: 'Successfully retrieved all Certificates',
-      data: Items?.map((item) => unmarshall(item)),
-      Items,
-    });
-  } catch (e) {
-    console.error(e);
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: 'Failed to retrieve posts.',
-      errorMsg: e.message,
-      errorStack: e.stack,
-    });
-  }
-  return response;
-};
-
+//exports methods globally
 module.exports = {
-  getAllCertificates,
-  getCertificate,
+  getCertificates,
 };
