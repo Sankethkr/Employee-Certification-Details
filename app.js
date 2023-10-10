891b6b5c9e47f5e1e285a67610488c1ddd72b7ef
Sankethkr24 <sanketh.kr@hyniva.com>
2023-10-10 17:52:32 +0530

    second commit

--- app.js
+++ app.js
@@ -11,44 +11,45 @@ const client = new DynamoDBClient();
 
 const getCertificates = async (event) => {
   const response = { statusCode: 200 };
-console.log('event', event);
-try {
-  if (event.pathParameters && event.pathParameters.empID) {
-    // If empID is provided in the path parameters, retrieve a specific certificate
-    const params = {
-      TableName: process.env.DYNAMODB_TABLE_NAME,
-      Key: marshall({ empID: event.pathParameters.empID }),
-    };
-    const { Item } = await client.send(new GetItemCommand(params));
-    console.log('Item', JSON.stringify(Item, null, 2));
+  console.log('event', event);
+  console.log('response', response);
+  try {
+    if (event.pathParameters && event.pathParameters.empID) {
+      // If empID is provided in the path parameters, retrieve a specific certificate
+      const params = {
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Key: marshall({ empID: event.pathParameters.empID }),
+      };
+      const { Item } = await client.send(new GetItemCommand(params));
+      console.log('Item', JSON.stringify(Item, null, 2));
 
-    response.body = JSON.stringify({
-      message: `Successfully retrieved empId: ${event.pathParameters.empID}`,
-      data: Item ? unmarshall(Item) : {},
-      rawData: Item,
-    });
-  } else {
-    // If empID is not provided, retrieve all certificates
-    const { Items } = await client.send(
-      new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
-    );
+      response.body = JSON.stringify({
+        message: `Successfully retrieved empId: ${event.pathParameters.empID}`,
+        data: Item ? unmarshall(Item) : {},
+        rawData: Item,
+      });
+    } else {
+      // If empID is not provided, retrieve all certificates
+      const { Items } = await client.send(
+        new ScanCommand({ TableName: process.env.DYNAMODB_TABLE_NAME })
+      );
 
+      response.body = JSON.stringify({
+        message: 'Successfully retrieved all Certificates',
+        data: Items?.map((item) => unmarshall(item)),
+        Items,
+      });
+    }
+  } catch (e) {
+    console.error(e);
     response.body = JSON.stringify({
-      message: 'Successfully retrieved all Certificates',
-      data: Items?.map((item) => unmarshall(item)),
-      Items,
+      message: 'Failed to get certificates.',
+      errorMsg: e.message,
+      errorStack: e.stack,
+      statusCode: e.$metadata.httpStatusCode,
     });
+    console.log(response.body);
   }
-} catch (e) {
-  console.error(e);
-  response.body = JSON.stringify({
-    message: 'Failed to get certificates.',
-    errorMsg: e.message,
-    errorStack: e.stack,
-    statusCode: e.$metadata.httpStatusCode,
-  });
-  console.log(response.body);
-}
 
   return response;
 };
