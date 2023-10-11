74a9c7b7e3535e7cc3e27c1a7cd40e0038391bb1
Sankethkr24 <sanketh.kr@hyniva.com>
2023-10-11 10:41:35 +0530

    delete functionality

--- app.js
+++ app.js
@@ -54,7 +54,93 @@ const getCertificates = async (event) => {
   return response;
 };
 
+const deleteEmployeeSkillInfo = async (event) => {
+  // defined const response and store the status code of 200
+  const response = { statusCode: 200 };
+  // try block will examine employeeId in DB and if found it will delete otherwise it will throw error
+  try {
+    const { empID } = event.pathParameters;
+
+    // Create an empty DynamoDB List attribute after delete perform
+    const emptyList = { L: [] };
+
+    // created const params and refered in program to proccess employeeId update
+    const params = {
+      // Table name
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ empID }),
+      UpdateExpression: 'SET bankInfoDetails = :emptyList',
+      ExpressionAttributeValues: {
+        ':emptyList': emptyList, //
+      },
+    };
+
+    // Use the update method with UpdateExpression to set bankInfoDetails to an empty list
+    const updateResult = await client.send(new UpdateItemCommand(params));
+
+    // convert raw data response from server to JSON string format
+    response.body = JSON.stringify({
+      message: `Successfully deleted employeeId bank Details.`,
+      updateResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    // convert raw data response from server to JSON string format
+    response.body = JSON.stringify({
+      message: `Failed to delete employeeId bank Details.`,
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  // returns the response 200
+  return response;
+};
+
+const softDeleteEmployeeSkillInfo = async (event) => {
+  // set 200 response
+  const response = { statusCode: 200 };
+  try {
+    const { empID } = event.pathParameters;
+    // writing params
+    const params = {
+      // table name
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      // passing marshalled employeeId value
+      Key: marshall({ employeeId }),
+      // update expression for isActive property which present in bankInfoDetails
+      UpdateExpression: 'SET bankInfoDetails[0].isActive = :isActive',
+      ExpressionAttributeValues: {
+        // Set to true to update "isActive" to true
+        ':isActive': { BOOL: true },
+      },
+    };
+
+    // sending params to dynamoDb
+    const updateResult = await client.send(new UpdateItemCommand(params));
+
+    // response body values
+    response.body = JSON.stringify({
+      message: `Successfully soft deleted employeeId bank Details.`,
+      updateResult,
+    });
+  } catch (e) {
+    // error handling block for 500 error satus
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: `Failed to soft delete employeeId bank Details.`,
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  // returns the response
+  return response;
+};
+
 //exports methods globally
 module.exports = {
   getCertificates,
+  deleteEmployeeSkillInfo,
+  softDeleteEmployeeSkillInfo,
 };
